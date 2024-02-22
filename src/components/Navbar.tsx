f14f95cb5b815badccbdd7460a4aba3a0b5a6421
crystal1229d <crystal1229d@naver.com>
2024-02-23 00:35:20 +0900

    모바일화면에서 Navbar 를 열고 아이템 클릭 시 Navbar 가 닫히게끔 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { useSession, signOut } from 'next-auth/react'
 import Link from 'next/link'
+import { useSession, signOut } from 'next-auth/react'
 
 import { BiMenu } from 'react-icons/bi'
 import { AiOutlineClose } from 'react-icons/ai'
@@ -12,8 +12,8 @@ export default function Navbar() {
   return (
     <>
       <div className="navbar">
-        <Link href="/" className="navbar__logo">
-          taste-roads
+        <Link className="navbar__logo" href="/">
+          nextmap
         </Link>
         <div className="navbar__list">
           <Link href="/stores" className="navbar__list--item">
@@ -51,26 +51,50 @@ export default function Navbar() {
       {isOpen && (
         <div className="navbar--mobile">
           <div className="navbar__list--mobile">
-            <Link href="/stores" className="navbar__list--item--mobile">
+            <Link
+              href="/stores"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               맛집 목록
             </Link>
-            <Link href="/stores/new" className="navbar__list--item--mobile">
+            <Link
+              href="/stores/new"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               맛집 등록
             </Link>
-            <Link href="/users/likes" className="navbar__list--item--mobile">
+            <Link
+              href="/users/likes"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               찜한 가게
             </Link>
-            <Link href="/users/mypage" className="navbar__list--item--mobile">
+            <Link
+              href="/users/mypage"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               마이페이지
             </Link>
             {status === 'authenticated' ? (
-              <button type="button" onClick={() => signOut()}>
+              <button
+                type="button"
+                onClick={() => {
+                  signOut()
+                  setIsOpen(false)
+                }}
+                className="navbar__list--item--mobile text-left"
+              >
                 로그아웃
               </button>
             ) : (
               <Link
                 href="/api/auth/signin"
                 className="navbar__list--item--mobile"
+                onClick={() => setIsOpen(false)}
               >
                 로그인
               </Link>
