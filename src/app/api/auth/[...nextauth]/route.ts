7b5e609bd8d1a9c54494b7cce0efcd6669145f0b
crystal1229d <crystal1229d@naver.com>
2024-03-06 14:20:43 +0900

    nextauth 수정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from '@/db'
 
 import GoogleProvider from 'next-auth/providers/google'
-import NaverProvider from 'next-auth/providers/naver'
+// import NaverProvider from 'next-auth/providers/naver'
 import KakaoProvider from 'next-auth/providers/kakao'
 import { Adapter } from 'next-auth/adapters'
 
@@ -19,10 +19,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID || '',
-      clientSecret: process.env.NAVER_CLIENT_SECRET || '',
-    }),
+    // NaverProvider({
+    //   clientId: process.env.NAVER_CLIENT_ID || '',
+    //   clientSecret: process.env.NAVER_CLIENT_SECRET || '',
+    // }),
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID || '',
       clientSecret: process.env.KAKAO_CLIENT_SECRET || '',
